6f0a11f56f57f023d657081c9db104917b628ac2
Erik Soderholm <esodey@gmail.com>
2019-04-15 12:31:03 -0700

    Finished the puesdoclassical super class and subclasses for beesbeesbees

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,15 @@
 var ForagerBee = function() {
+    HoneyMakerBee.call(this);
+    this.job = 'find pollen';
+    this.canFly = true;
+    this.treasureChest = [];
 };
 
+ForagerBee.prototype = Object.create(Grub.prototype);
+ForagerBee.prototype = Object.create(HoneyMakerBee.prototype);
+
+ForagerBee.prototype.forage = function () {
+    this.treasureChest.push('treasure');
+};
+
+ForagerBee.prototype.constructor = ForagerBee;
